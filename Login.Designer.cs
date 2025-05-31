3bb0fb5da5d4084f35df2ada3d88b9cb9dfe99c3
HP <HP@DESKTOP-L0TTRFA>
2025-06-01 02:18:23 +1200

    Ankush- Login form ready

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -28,29 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.linkLabelForgot = new System.Windows.Forms.LinkLabel();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.linkLabelRegister = new System.Windows.Forms.LinkLabel();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // linkLabelForgot
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(74, 62);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.linkLabelForgot.AutoSize = true;
+            this.linkLabelForgot.LinkColor = System.Drawing.Color.RosyBrown;
+            this.linkLabelForgot.Location = new System.Drawing.Point(591, 357);
+            this.linkLabelForgot.Name = "linkLabelForgot";
+            this.linkLabelForgot.Size = new System.Drawing.Size(114, 13);
+            this.linkLabelForgot.TabIndex = 70;
+            this.linkLabelForgot.TabStop = true;
+            this.linkLabelForgot.Text = "Forgot your password?";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxEmail.Location = new System.Drawing.Point(79, 212);
+            this.textBoxEmail.Multiline = true;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(639, 41);
+            this.textBoxEmail.TabIndex = 69;
+            this.textBoxEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxPassword.Location = new System.Drawing.Point(82, 309);
+            this.textBoxPassword.Multiline = true;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(639, 41);
+            this.textBoxPassword.TabIndex = 68;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(370, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 21);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 67;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(222, 81);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(361, 44);
+            this.label9.TabIndex = 66;
+            this.label9.Text = "Login to Dream Motors";
+            // 
+            // linkLabelRegister
+            // 
+            this.linkLabelRegister.AutoSize = true;
+            this.linkLabelRegister.LinkColor = System.Drawing.Color.RosyBrown;
+            this.linkLabelRegister.Location = new System.Drawing.Point(319, 456);
+            this.linkLabelRegister.Name = "linkLabelRegister";
+            this.linkLabelRegister.Size = new System.Drawing.Size(167, 13);
+            this.linkLabelRegister.TabIndex = 65;
+            this.linkLabelRegister.TabStop = true;
+            this.linkLabelRegister.Text = "Don\'t have an account ? Register";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonLogin.Location = new System.Drawing.Point(79, 401);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(639, 43);
+            this.buttonLogin.TabIndex = 64;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(285, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(234, 13);
+            this.label8.TabIndex = 63;
+            this.label8.Text = "Enter Your Credentials To Access Your Account";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(83, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 62;
+            this.label4.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 61;
+            this.label3.Text = "Email";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 527);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.linkLabelForgot);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.linkLabelRegister);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +167,15 @@
 
         #endregion
 
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.LinkLabel linkLabelForgot;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.LinkLabel linkLabelRegister;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Login.cs
+++ Login.cs
@@ -11,7 +11,10 @@ using System.Windows.Forms;
 namespace DreamMotors
 {
     public partial class Login : Form
+
     {
+
+       
         public Login()
         {
             InitializeComponent();
@@ -19,7 +22,32 @@ namespace DreamMotors
 
         private void Login_Load(object sender, EventArgs e)
         {
+            string enteredEmail = textBoxEmail.Text.Trim();
+            string enteredPassword = textBoxPassword.Text;
+
+            Dictionary<string, string> userCredentials = new Dictionary<string, string>()
+    {
+        { "admin@gmail.com", "admin123" },
+        { "user1@gmail.com", "123" },
+        { "user2@gmail.com", "123" },
+        { "user3@gmail.com", "123" },
+        { "user4@gmail.com", "123" }
+    };
+
+            if (userCredentials.ContainsKey(enteredEmail) && userCredentials[enteredEmail] == enteredPassword)
+            {
+                // Open Dashboard and hide Login form
+                Dashboard dashboard = new Dashboard();
+                dashboard.Show();
+                this.Hide(); // Hides the login form
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
-}k
+}
