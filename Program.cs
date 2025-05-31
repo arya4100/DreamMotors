a475d0cd30b033d5aef7c2c93f45029821a91e2e
Kiranpreetkaur12 <kiranpreetkaur2086@gmail.com>
2025-06-01 00:10:04 +1200

    Kiran reset form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace DreamMotors
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DashBoard());
+            Application.Run(new Reset());
         }
     }
 }
