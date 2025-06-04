fb1da459b133f32331ae02a736f937129798ec1a
arya4100 <aryapatel4100@gmail.com>
2025-06-04 19:16:46 +1200

    ARYA

--- DashBoard.Designer.cs
+++ DashBoard.Designer.cs
@@ -203,7 +203,7 @@ namespace DreamMotors
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1484, 709);
+            this.ClientSize = new System.Drawing.Size(1587, 778);
             this.Controls.Add(this.btnCreateaccount);
             this.Controls.Add(this.btnBrowseinventory);
             this.Controls.Add(this.textBox2);
