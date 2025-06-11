36b8de5d6c5df520de95b9d0adbdd0a1ef119ef4
arya4100 <aryapatel4100@gmail.com>
2025-06-11 17:47:25 +1200

    Arya- Done Redirect Code For Camry

--- Inventory.cs
+++ Inventory.cs
@@ -1,5 +1,4 @@
-﻿// Inventory.cs
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -40,7 +39,7 @@ namespace DreamMotors
             carList.Add(new Car { Name = "2023 Toyota Camry", Price = 28000, Image = Properties.Resources.camry });
             carList.Add(new Car { Name = "2023 Toyota Corolla", Price = 23000, Image = Properties.Resources.corolla });
             carList.Add(new Car { Name = "2023 Toyota RAV4", Price = 32000, Image = Properties.Resources.rav4 });
-            carList.Add(new Car { Name = "2023 Toyota Aqua", Price = 23000, Image = Properties.Resources.aqua});
+            carList.Add(new Car { Name = "2023 Toyota Aqua", Price = 23000, Image = Properties.Resources.aqua });
             carList.Add(new Car { Name = "2023 Toyota Fortuner", Price = 53000, Image = Properties.Resources.Fortuner });
             carList.Add(new Car { Name = "2023 Toyota Prius", Price = 26000, Image = Properties.Resources.prius });
             carList.Add(new Car { Name = "2023 Toyota Yaris", Price = 20000, Image = Properties.Resources.yaris });
@@ -55,11 +54,58 @@ namespace DreamMotors
 
             foreach (var car in cars)
             {
-                Panel carPanel = new Panel { Width = 250, Height = 320, Margin = new Padding(10), BorderStyle = BorderStyle.FixedSingle };
-                PictureBox pb = new PictureBox { Image = car.Image, SizeMode = PictureBoxSizeMode.StretchImage, Width = 230, Height = 150, Location = new Point(10, 10) };
-                Label lblName = new Label { Text = car.Name, Location = new Point(10, 170), AutoSize = true, Font = new Font("Arial", 10, FontStyle.Bold) };
-                Label lblPrice = new Label { Text = "$ " + car.Price.ToString("N0"), Location = new Point(10, 195), AutoSize = true, Font = new Font("Arial", 10) };
-                Button btnView = new Button { Text = "View Details", Width = 230, Location = new Point(10, 230), BackColor = Color.Black, ForeColor = Color.White };
+                Panel carPanel = new Panel
+                {
+                    Width = 250,
+                    Height = 320,
+                    Margin = new Padding(10),
+                    BorderStyle = BorderStyle.FixedSingle
+                };
+
+                PictureBox pb = new PictureBox
+                {
+                    Image = car.Image,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Width = 230,
+                    Height = 150,
+                    Location = new Point(10, 10)
+                };
+
+                Label lblName = new Label
+                {
+                    Text = car.Name,
+                    Location = new Point(10, 170),
+                    AutoSize = true,
+                    Font = new Font("Arial", 10, FontStyle.Bold)
+                };
+
+                Label lblPrice = new Label
+                {
+                    Text = "$ " + car.Price.ToString("N0"),
+                    Location = new Point(10, 195),
+                    AutoSize = true,
+                    Font = new Font("Arial", 10)
+                };
+
+                Button btnView = new Button
+                {
+                    Text = "View Details",
+                    Width = 230,
+                    Location = new Point(10, 230),
+                    BackColor = Color.Black,
+                    ForeColor = Color.White
+                };
+
+                if (car.Name.Contains("Camry"))
+                {
+                    btnView.Click += (s, e) =>
+                    {
+                        ViewDetail detailsForm = new ViewDetail();
+                        detailsForm.Show();
+                        this.Hide();
+
+                    };
+                }
 
                 carPanel.Controls.Add(pb);
                 carPanel.Controls.Add(lblName);
