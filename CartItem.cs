061dd49119741e4aa4d47e319654ee6393b7973e
arya4100 <aryapatel4100@gmail.com>
2025-06-14 00:07:26 +1200

    Arya Done View Detail And Implemented Cart Fully.

---
+++ CartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace DreamMotors
+{
+    public class CartItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} x {Quantity} - ${Price * Quantity:0.00}";
+        }
+    }
+}
